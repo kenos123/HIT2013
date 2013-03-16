e6fa39f6e5e86a9e47532bdf1ff9be9c8295b09a
Ivan Doležal <ivan.dolezal@gmail.com>
2013-03-16 16:42:06 +0100

    UPDATE

--- demo/src/main/java/com/generalidevelopment/hit2013/domain/Item.java
+++ demo/src/main/java/com/generalidevelopment/hit2013/domain/Item.java
@@ -2,14 +2,19 @@ package com.generalidevelopment.hit2013.domain;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "ITEM")
 public class Item {
 
 	@NotNull
@@ -22,6 +27,10 @@ public class Item {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
 	private Long id;
+
+	@Lob
+	@Basic(fetch = FetchType.LAZY)
+	private byte[] image;
 
 	public String getName() {
 		return this.name;
@@ -45,6 +54,20 @@ public class Item {
 
 	public void setId(final Long id) {
 		this.id = id;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public byte[] getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(final byte[] image) {
+		this.image = image;
 	}
 
 }
--- demo/src/main/java/com/generalidevelopment/hit2013/web/ItemController.java
+++ demo/src/main/java/com/generalidevelopment/hit2013/web/ItemController.java
@@ -1,24 +1,32 @@
 package com.generalidevelopment.hit2013.web;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 import org.springframework.web.util.UriUtils;
 import org.springframework.web.util.WebUtils;
 
 import com.generalidevelopment.hit2013.domain.Item;
 import com.generalidevelopment.hit2013.repository.ItemRepository;
 import com.generalidevelopment.hit2013.util.AppContext;
+import com.generalidevelopment.hit2013.util.ImageUtils;
 import com.generalidevelopment.hit2013.util.json.JSONResponseEnvelope;
 
 @RequestMapping("/items")
@@ -26,7 +34,19 @@ import com.generalidevelopment.hit2013.util.json.JSONResponseEnvelope;
 public class ItemController {
 
 	@Autowired
-	ItemRepository itemRepository;
+	private ItemRepository itemRepository;
+
+	@Value("${app.thumbnail.width}")
+	private Long thumbWidth;
+
+	@Value("${app.thumbnail.height}")
+	private Long thumbHeight;
+
+	@InitBinder
+	protected void initBinder(final HttpServletRequest request, final ServletRequestDataBinder binder)
+			throws ServletException {
+		binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
+	}
 
 	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
 	public String create(@Valid final Item item, final BindingResult bindingResult, final Model uiModel,
@@ -51,6 +71,40 @@ public class ItemController {
 		uiModel.addAttribute("item", itemRepository.findOne(id));
 		uiModel.addAttribute("itemId", id);
 		return "items/show";
+	}
+
+	@RequestMapping(value = "/{id}", params = "image", produces = "image/jpg")
+	public @ResponseBody
+	byte[] image(@PathVariable("id") final Long id, final Model uiModel) {
+		final Item item = itemRepository.findOne(id);
+		if (item != null) {
+			final ByteArrayOutputStream bao = new ByteArrayOutputStream();
+			try {
+				bao.write(item.getImage());
+			} catch (final IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return bao.toByteArray();
+		}
+		return null;
+	}
+
+	@RequestMapping(value = "/{id}", params = "thumb", produces = "image/jpg")
+	public @ResponseBody
+	byte[] thumb(@PathVariable("id") final Long id, final Model uiModel) {
+		final Item item = itemRepository.findOne(id);
+		if (item != null) {
+			final ByteArrayOutputStream bao = new ByteArrayOutputStream();
+			try {
+				bao.write(ImageUtils.scaleJpg(item.getImage(), thumbWidth.intValue(), thumbHeight.intValue()));
+			} catch (final IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return bao.toByteArray();
+		}
+		return null;
 	}
 
 	@RequestMapping(produces = "text/html")
